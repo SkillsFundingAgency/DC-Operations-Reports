6cc2759e4c8e6a8c324e653b99b6bb5bb12ade97
Hari Dupati <dupatihari@gmail.com>
2020-08-11 08:42:47 +0100

    103498 - Rule Validation Detail Report 2021

--- src/ESFA.DC.Operations.Reports.Stateless/Config/ReportServiceConfiguration.cs
+++ src/ESFA.DC.Operations.Reports.Stateless/Config/ReportServiceConfiguration.cs
@@ -7,6 +7,8 @@ namespace ESFA.DC.Operations.Reports.Stateless.Config
         public string IlrDataStore1819ConnectionString { get; set; }
 
         public string IlrDataStore1920ConnectionString { get; set; }
+        
+        public string IlrDataStore2021ConnectionString { get; set; }
 
         public string PimsDataConnectionString { get; set; }
     }
